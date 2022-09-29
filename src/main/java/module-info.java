52b0d148dc73bb592526a4f8fbb7ddc527275bc6
Costa Varyn <d3tug@yandex.ru>
2022-09-29 15:40:11 +0300

    add module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,18 @@
 module com.example.simpledraft {
     requires javafx.controls;
     requires javafx.fxml;
-        requires javafx.web;
-            
-        requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-            requires validatorfx;
-            requires org.kordamp.ikonli.javafx;
-            requires org.kordamp.bootstrapfx.core;
-            requires eu.hansolo.tilesfx;
-            requires com.almasb.fxgl.all;
-    
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
     opens com.example.simpledraft to javafx.fxml;
     exports com.example.simpledraft;
+    exports;
+    opens to
 }

73c3544686b105362f2673e44b7c405b659c66a1
Costa Varyn <d3tug@yandex.ru>
2022-09-29 16:16:51 +0300

    add files

---
+++ src/main/java/com/example/SimpleDraft/AppFX.java
@@ -0,0 +1,42 @@
+package com.example.SimpleDraft;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class AppFX extends Application {
+
+    public static void main(String[] args) {
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Dogs application");
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(400);
+
+        InputStream iconStream =
+                getClass().getResourceAsStream("/images/someImage.png");
+        Image image = new Image(iconStream);
+        primaryStage.getIcons().add(image);
+
+        Button button = new Button("WOF WOF ???'");
+/**
+        button.setOnAction(e -< {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, "WOF WOF WOF!!!");
+        alert.showAndWait();
+    })*/
+        Scene primaryScene = new Scene(button);
+        primaryStage.setScene(primaryScene);
+
+        primaryStage.show();
+    }
+}
